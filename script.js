dda812897db90da086d84bf3a3e20051b3098860
Windows 11 <win12320241@outlook.com>
2025-11-22 19:16:33 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ client.on('open', () => {
 	client.send({m: "login", type: "token", name: localStorage.name, token: localStorage.token, id: localStorage.id})
 })
 client.on('login', msg => {
-	if (!msg.login) return window.location.assign(`/login/#${encodeURIComponent(location.href.substr(location.origin.length))}`);
+	// if (!msg.login) return window.location.assign(`/login/#${encodeURIComponent(location.href.substr(location.origin.length))}`); // Temporarily disabled for testing
 	document.getElementById('status').textContent = `Logged in as ${msg.name} [${msg.id}]`
 })
